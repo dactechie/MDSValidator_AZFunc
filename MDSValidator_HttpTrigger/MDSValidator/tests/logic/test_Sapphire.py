5f5b9ba537ca6cc61a9c911a9edc3bf1c35d6353
Aftab.Jalal <directionshealth.forms@gmail.com>
2021-09-07 12:48:41 +1200

    updated NSW definitions to match data dictionary

--- MDSValidator_HttpTrigger/MDSValidator/tests/logic/test_Sapphire.py
+++ MDSValidator_HttpTrigger/MDSValidator/tests/logic/test_Sapphire.py
@@ -35,5 +35,28 @@ def test_noerror_prev_trmt(NSWCCARE_json_validator, client_id, prev_trmt, err):
   
 
   assert (not err and len(errors[0]) == 0)  or errors[0][0] == err
+
+
+  
+@pytest.mark.parametrize("client_id, drug, method, err", [
+                                          ('11525',  'gambling', 'not collected', {}),
+                                          #  ('4353', 'information and education', '', {}),
+                                          #('343','pharmacotherapy', '', error),
+                                          # ('11526','pharmacotherapy', 'support and case management', {}),
+                                          # ('11525','support and case management', 'pharmacotherapy', {}),
+                                          ])
+def test_nsw_method_of_use_matrix(NSWCCARE_json_validator, client_id, drug, method, err):
+  base1_w_error = copy.deepcopy(noerrors_base_nsw_translated)
+  base1_w_error[M['PDC']] =  drug
+  base1_w_error[M['METHOD']] =  method
+  base1_w_error['id'] = client_id
+
+  if err:
+    err['cid'] = client_id
+  
+  errors, _ = NSWCCARE_json_validator.validate({'episodes' :[base1_w_error]})
+  
+
+  assert (not err and len(errors[0]) == 0)  or errors[0][0] == err
   
 
